79f3195e1eb58e8a035751d6c4c121009503b6dc
Kevin Emunds <Kevin.Emunds_1@student.hs-rm.de>
2022-11-09 21:11:37 +0100

    Projektstruktur: Spring + Vue Projekt angelegt

---
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swt_project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
