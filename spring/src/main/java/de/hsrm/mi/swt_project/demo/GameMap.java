b3603eb51374427031ec191a4b7b04b1e59163c3
Tom Gouthier <tom.gouthier@student.hs-rm.de>
2022-11-12 17:22:17 +0100

    created further interfaces. BLI-45

---
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/GameMap.java
@@ -0,0 +1,28 @@
+package de.hsrm.mi.swt_project.demo;
+
+import java.util.List;
+
+/**
+ * Represents a 2d field of tiles and its npcs
+@author Tom Gouthier
+ */
+public class GameMap {
+
+    private Tile[][] tiles;
+    private String name;
+    private List <MoveableObject> npcs;
+
+    private void addNpc(MoveableObject moveable){
+
+    }
+
+    private void addTile(Tile tile, int xPos, int yPos){
+
+    }
+
+    private void expandMap(){
+
+    }
+
+    
+}
