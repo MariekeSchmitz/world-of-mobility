7343412860cd8e048028ac49ab1a9a07e1e22263
Tom Gouthier <tom.gouthier@student.hs-rm.de>
2022-11-13 18:09:32 +0100

    added TileClasses BLI-45

--- spring/src/main/java/de/hsrm/mi/swt_project/demo/Tile.java
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/Tile.java
@@ -1,5 +1,23 @@
 package de.hsrm.mi.swt_project.demo;
 
-public class Tile {
+import java.util.List;
+
+public abstract class Tile implements Turnable{
     
+    protected Orientation orientation;
+    protected List <Placeable> placedObjects;
+    protected TileType type;
+
+
+    public void addPlaceable(Placeable placeable) {
+
+    }
+
+
+    @Override
+    public void turn(Direction direction) {
+
+    }
+
+
 }
