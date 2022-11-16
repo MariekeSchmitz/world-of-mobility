6da42dd09da72d7d6485305669963fb699076e7f
Tom Gouthier <tom.gouthier@student.hs-rm.de>
2022-11-16 15:26:38 +0100

    BLI-45 REFACTOR: added javadoc for dto

--- spring/src/main/java/de/hsrm/mi/swt_project/demo/GetGameCommandDTO.java
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/GetGameCommandDTO.java
@@ -1,4 +1,8 @@
 package de.hsrm.mi.swt_project.demo;
 
+/**
+ * Data Transfer Object that the Server receives by the Client on every game command by a user
+ * @author Tom Gouthier
+ */
 public record GetGameCommandDTO(String user, GameControl control) {}
 
