482e13401aa4a70d8d2652e291156c63caa73e35
Victoria Thee <victoria.thee@student.hs-rm.de>
2022-11-18 19:01:23 +0100

    BLI-59 FEATURE: new tile types available

---
+++ vue/src/components/Tiles/RAIL_STRAIGHT.vue
@@ -0,0 +1,41 @@
+<template>
+    <Plane
+      :width="props.width"
+      :height="props.height"
+      :rotation="{ x: props.rotationX, y: props.rotationY, z: props.rotationZ }"
+      :position="{ x: props.posX, y: props.posY, z: props.posZ }"
+    >
+      <ToonMaterial>
+        <Texture src="src\textures\tiles\RAIL_STRAIGHT.jpg"
+      /></ToonMaterial>
+    </Plane>
+    <!-- place for objects that are meant for this tile  -->
+  </template>
+  
+  <script setup lang="ts">
+  //--------imports-----------
+  
+  import { defineProps } from "vue";
+  import {
+    ToonMaterial,
+    Plane,
+    Texture,
+  } from "troisjs";
+  
+  //--------props-------------
+  
+  // rotation: to translate our 'orientation' to a matching Tile, we need to pass a multiple 'Math.PI / 2' to our rotationZ value;
+  const props = defineProps<{
+    width: number;
+    height: number;
+    posX: number;
+    posY: number;
+    posZ: number;
+    rotationX: number;
+    rotationY: number;
+    rotationZ: number;
+    type: string;
+  }>();
+  
+  //--------code------------
+  </script>
