2770201dad4640508de0374314ec97762029c172
Kevin Emunds <Kevin.Emunds_1@student.hs-rm.de>
2022-11-19 11:23:01 +0100

    BLI-59 RENAME: Rename to prep for merg

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from './TestApp.vue'
 import router from './router'
 import GamingMap from '@/components/GamingMap.vue'
 
--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import GamingMap from '@/components/GamingMap.vue'
-import App from '@/App.vue'
+import App from '@/TestApp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
