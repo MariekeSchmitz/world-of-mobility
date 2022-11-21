efe5d2c48c5bee494ee79745c7ce79307f30efd3
Beate Arnold <beate.arnold@student.hs-rm.de>
2022-11-21 20:38:20 +0100

    BLI-04 FEATURE: Add Cars Rotation

--- vue/src/TestCompBeateMarie2.vue
+++ vue/src/TestCompBeateMarie2.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref, onMounted, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 import {
   AmbientLight,
-  Box,
   Camera,
+  Box,
   ToonMaterial,
   PointLight,
   Renderer,
@@ -11,58 +11,91 @@ import {
   Scene,
 } from "troisjs";
 
+import { Vector3, MathUtils } from "three";
+
 import Car from "./components/Car.vue";
 
 const rendererC = ref();
 
-const positionTemp = reactive({
-  x: 0,
-  y: 0,
-  z: -2.5,
-});
+const forwardVec = new Vector3(0, 1, 0);
 
-const rotation = reactive({
-  x: 0,
-  y: 0,
-  z: 0
-})
+const positionTemp = reactive(new Vector3(0, 0, -2.5));
+
+//direction value is a value between 0 and 7
+//0 -> north
+//1 -> northWest
+//... -> leftTurn : direction + 2 % 8 (expl. from north to west)
+//counter-clockwise (->like radians)
+const direction = ref(0);
 
 document.addEventListener("keyup", (e) => {
   if (e.code === "KeyW") {
-    positionTemp.y += 1;
-    console.log(`y: ${positionTemp.y}`)
+    const rotatedForwardVec = rotate(forwardVec, rotation.value);
+    positionTemp.add(rotatedForwardVec);
   } else if (e.code === "KeyS") {
-    positionTemp.y -= 1;
+    const rotatedForwardVec = rotate(forwardVec, rotation.value);
+    positionTemp.sub(rotatedForwardVec);
   } else if (e.code === "KeyD") {
-    rotation.y += 1;
-    console.log(`x: ${positionTemp.x}`)
+    direction.value = (direction.value - 2) % 8;
   } else if (e.code === "KeyA") {
-    rotation.y -= 1;
-    console.log(`x: ${positionTemp.x}`)
+    direction.value = (direction.value + 2) % 8;
   }
 });
 
+//same Code as in Car Component
+function rotate(offsetVec: Vector3, angle: number) {
+  const axis = new Vector3(0, 0, 1);
+  const w = offsetVec.clone();
+  const res = w.applyAxisAngle(axis, angle);
+  return res;
+}
 
+const pi = MathUtils.degToRad(180);
+
+const rotation = computed(() => {
+  return (direction.value * pi) / 4;
+}); //rotation in radians
 </script>
 
 <template>
   <Renderer ref="rendererC" antialias resize="window">
-    <Camera :position="{ x:  0, y: positionTemp.y -12 , z: 0}" :lookAt="{ x: 0 , y: positionTemp.y, z: 0}"  ref="camera" />
+    <Camera
+      :position="{ x: 0, y: positionTemp.y - 12, z: 0 }"
+      :lookAt="{ x: 0, y: positionTemp.y, z: 0 }"
+      ref="camera"
+    />
     <Scene background="#97FFFF">
       <!-- Licht -->
       <PointLight :position="{ x: 0, y: 0, z: 10 }" />
       <AmbientLight :intensity="1" color="#ff6000"></AmbientLight>
 
-      <Car :x="positionTemp.x" :y="positionTemp.y" :z="positionTemp.z"></Car>
-   
+      <Car :pos="positionTemp" :rotation="rotation"></Car>
+
+      <!-- <Box :position="positionTemp" :size="0.5"> </Box> -->
+
       <!-- "Fahrbahn" -->
-      <Plane :width="0.2" :height="2" :rotation="{ x: 0 }" :position="{ y: -3, z: -2.99 }">
+      <Plane
+        :width="0.2"
+        :height="2"
+        :rotation="{ x: 0 }"
+        :position="{ y: -3, z: -2.99 }"
+      >
         <ToonMaterial color="#FFFFFF" />
       </Plane>
-      <Plane :width="0.2" :height="2" :rotation="{ x: 0 }" :position="{ z: -2.99 }">
+      <Plane
+        :width="0.2"
+        :height="2"
+        :rotation="{ x: 0 }"
+        :position="{ z: -2.99 }"
+      >
         <ToonMaterial color="#FFFFFF" />
       </Plane>
-      <Plane :width="0.2" :height="2" :rotation="{ x: 0 }" :position="{ y: 3, z: -2.99 }">
+      <Plane
+        :width="0.2"
+        :height="2"
+        :rotation="{ x: 0 }"
+        :position="{ y: 3, z: -2.99 }"
+      >
         <ToonMaterial color="#FFFFFF" />
       </Plane>
       <Plane :width="8" :height="8" :rotation="{ x: 0 }" :position="{ z: -3 }">
@@ -72,6 +105,4 @@ document.addEventListener("keyup", (e) => {
   </Renderer>
 </template>
 
-<style>
-
-</style>
+<style></style>
