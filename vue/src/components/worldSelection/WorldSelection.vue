8b19a03135683fed60d4784fb6479212e475f052
Marieke Schmitz <marieke.schmitz@student.hs-rm.de>
2022-11-30 12:49:16 +0100

    BLI-8 #67 FEATURE: Created WorldSelection-View with test-JSON

---
+++ vue/src/components/worldSelection/WorldSelection.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+import { ref } from "vue";
+import { useMapOverview } from "@/services/useMapOverview";
+
+const { mapsOverview } = useMapOverview()
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <h1>Welt für neues Spiel wählen</h1>
+        <ul>
+            <div class ="square" v-for="map in (mapsOverview.maps)">
+            
+            </div>
+        </ul>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+.square {
+    width: 12.5rem;
+    height: 15rem;
+    background-color: red;
+}
+.wrapper {
+  display: grid;
+  place-items: center;
+}
+
+button {
+  padding: 1em;
+  border-radius: 1em;
+  background-color: antiquewhite;
+  width: 10rem;
+  margin: 10px;
+}
+</style>
--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Game from "@/components/Game.vue";
 import WorldSelect from "@/views/WorldSelect.vue";
+import WorldSelection from "@/components/worldSelection/WorldSelection.vue";
 import GameSelect from "@/views/GameSelect.vue";
 
 const router = createRouter({
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/gameselect",
       name: "GameSelect",
       component: GameSelect,
+    },
+    {
+      path: "/worldSelection",
+      name: "WorldSelection",
+      component: WorldSelection,
     },
   ],
 });
