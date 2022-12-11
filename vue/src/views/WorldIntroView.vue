791c80097004a8e3fffdd425248dfc619bd4c725
Astrid Klemmer <astrid.klemmer@student.hs-rm.de>
2022-12-11 12:24:11 +0100

    BLI_38_#84 REFACTOR: Maps create new EditorInstance and forward to EditorView

--- vue/src/views/WorldIntroView.vue
+++ vue/src/views/WorldIntroView.vue
@@ -1,78 +1,103 @@
 <script setup lang="ts">
-    import GameListItem from '@/components/selectview/GameListItem.vue';
-    import { useInstanceList } from "@/services/useInstanceList"
-    import { computed, ref } from '@vue/reactivity';
-    import { onMounted } from 'vue';
-    import { useMapOverview } from "@/services/useMapOverview";
-    
-    const { instanceState, getInstanceList } = useInstanceList();
-    const { mapsOverview, getMaps } = useMapOverview()
+import GameListItem from "@/components/selectview/GameListItem.vue";
+import { useInstanceList } from "@/services/useInstanceList";
+import { computed, ref } from "@vue/reactivity";
+import { onMounted } from "vue";
+import { useMapOverview } from "@/services/useMapOverview";
+import { useEditor } from "@/services/useEditor";
+import { RouterLink } from "vue-router";
+import router from "@/router";
 
-    onMounted(() => {
-        getInstanceList("editor")
-        getMaps()  
-    });
+const { instanceState, getInstanceList } = useInstanceList();
+const { mapsOverview, getMaps } = useMapOverview();
+const { createWorld } = useEditor();
 
-    const showAll = ref(true);
+onMounted(() => {
+  getInstanceList("editor");
+  getMaps();
+});
 
-    function switchScene(mode:string){
-        if(mode == "all"){
-            showAll.value = true;
-        }
-        if(mode == "edit"){
-            showAll.value = false;
-        }
-    }
-    
+const showAll = ref(true);
 
+function switchScene(mode: string) {
+  if (mode == "all") {
+    showAll.value = true;
+  }
+  if (mode == "edit") {
+    showAll.value = false;
+  }
+}
 
+async function getWorldAndForwardToEditor(name: string) {
+  const id = await createWorld(name);
+  router.push(`/editor/${id}`);
+}
 </script>
 
 <template>
+  <div>
     <div>
-        <div>
-            <RouterLink to="/login"><img src="../buttons/editor/arrow-left.png" alt=""></RouterLink>
-            <RouterLink to="/createWorld">
-                <button>
-                    <img src="../buttons/editor/plus.png" alt=""> Welt erstellen
-                </button>
-            </RouterLink>
-            <img id="logo" src="" alt="Logo Fehlt">
-        </div>
-
-        <hr>
-
-        <div id="selection">
-            <h1>Welt editieren</h1>
-            <fieldset>
-                <input type="radio" id="editmode" name="selectmode" value="Wird gerade editiert" @click="switchScene('edit')">
-                <label for="editmode">Wird gerade editiert</label>
-                <input type="radio" id="allmode" name="selectmode" checked value="Alle" @click="switchScene('all')">
-                <label for="allmode">Alle</label>
-            </fieldset>
-     
-            <div v-for="ele in instanceState.instancelist.instancelist">
-                <GameListItem :gamename="ele.gamename" :worldname="ele.worldname"  :people="ele.playeramount"></GameListItem>
-            </div>
-
-            <div v-if="showAll" v-for="ele in mapsOverview.allMaps">
-                <GameListItem :worldname="ele.mapName"></GameListItem>
-            </div>
-
-        </div>
+      <RouterLink to="/login"
+        ><img src="../buttons/editor/arrow-left.png" alt=""
+      /></RouterLink>
+      <RouterLink to="/createWorld">
+        <button>
+          <img src="../buttons/editor/plus.png" alt="" /> Welt erstellen
+        </button>
+      </RouterLink>
+      <img id="logo" src="" alt="Logo Fehlt" />
     </div>
+
+    <hr />
+
+    <div id="selection">
+      <h1>Welt editieren</h1>
+      <fieldset>
+        <input
+          type="radio"
+          id="editmode"
+          name="selectmode"
+          value="Wird gerade editiert"
+          @click="switchScene('edit')"
+        />
+        <label for="editmode">Wird gerade editiert</label>
+        <input
+          type="radio"
+          id="allmode"
+          name="selectmode"
+          checked
+          value="Alle"
+          @click="switchScene('all')"
+        />
+        <label for="allmode">Alle</label>
+      </fieldset>
+
+      <div v-for="ele in instanceState.instancelist.instancelist">
+        <RouterLink :to="{ path: '/editor/' + ele.id }">
+          <GameListItem
+            :gamename="ele.gamename"
+            :worldname="ele.worldname"
+            :people="ele.playeramount"
+          ></GameListItem>
+        </RouterLink>
+      </div>
+
+      <div v-if="showAll" v-for="ele in mapsOverview.allMaps">
+        <button @click="getWorldAndForwardToEditor(ele.mapName)">
+          <GameListItem :worldname="ele.mapName"></GameListItem>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-
 #logo {
-    width: 5rem;
-    height: 5rem;
+  width: 5rem;
+  height: 5rem;
 }
 
 button {
-    text-align:center;
+  text-align: center;
 }
-
-
 </style>
