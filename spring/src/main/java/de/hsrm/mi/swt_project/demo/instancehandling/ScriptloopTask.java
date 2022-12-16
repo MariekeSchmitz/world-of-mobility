01cd687631de97ef2374cff35866ae89bbed8346
Sascha Scheid <sascha.scheid@student.hs-rm.de>
2022-12-16 16:43:42 +0100

    BLI-20_#116 REFACTOR: Move periodic execution of scripts into own task file

--- spring/src/main/java/de/hsrm/mi/swt_project/demo/instancehandling/ScriptloopTask.java
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/instancehandling/ScriptloopTask.java
@@ -6,29 +6,29 @@ import org.springframework.stereotype.Component;
 
 /**
  * If scheduling is enabled, this class will periodically
- * trigger the instance handler to update the instances 
- * and publish the current state to the corresponding 
- * clients.
+ * trigger the instance handler to trigger execution of
+ * of scripts. 
  * 
- * The rate is at which updates are triggered can be 
- * configured externally via updateloop.tickrate and
+ * The rate is at which script execution is triggered can 
+ * be configured externally via scriptloop.tickrate and
  * is measured in milliseconds.
  * 
  * If no external configuration is present, the
  * tickrate will fall back to the default rate
- * of 50 milliseconds.
+ * of 2000 milliseconds = 2 seconds.
  * 
- * @author Sascha Scheid 
+ * @author Sascha Scheid
+ * @author Finn Schindel
  */
 @Component
-public class UpdateloopTask {
-    
+public class ScriptloopTask {
+
     @Autowired
     private InstanceHandler instanceHandler;
 
-    @Scheduled(fixedRateString = "${updateloop.tickrate:50}")
-    public void triggerUpdate() {
-        instanceHandler.update();
+    @Scheduled(fixedRateString = "${skriptloop.tickrate:2000}")
+    public void triggerScriptExecution() {
+        instanceHandler.triggerScript();
     }
-
+    
 }
