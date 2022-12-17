9457e158f044bb07a0f3271bb3d0a31ccb395020
Felix Ruf <felix.ruf@student.hs-rm.de>
2022-12-17 19:49:11 +0100

    BLI-11 FEATURE: Added a simplified version of tiles for minimap purposes

---
+++ vue/src/components/simplifiedTile/SimplifiedTile.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="simplified-tile"></div>
+</template>
+
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+    tileType: string
+}>();
+
+
+
+</script>
+
+
+<style scoped>
+
+#simplified-tile {
+    background-image: url("@/textures/tiles/GRASSTILE.jpg");
+}
+
+</style>
