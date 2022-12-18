de427112a5bd9110cfee5fa0d46a1e4e24f56e92
Finn Schindel <finn.schindel@student.hs-rm.de>
2022-12-18 12:25:43 +0100

    BLI-20_#116 REFEATUR Pythoninterpretor / ScriptAPI

--- spring/src/main/resources/DefaultNPCScript.py
+++ spring/src/main/resources/DefaultNPCScript.py
@@ -1,14 +1,7 @@
+npc = NPC()
 
-def setSpeed(currentVelocity):
-    moveable.setCurrentVelocity(currentVelocity)
+npc.setSpeed(0.1)
 
-def turnLeft():
-    moveable.turn(Direction.LEFT)
-
-def turnRight():
-    moveable.turn(Direction.RIGHT)
-
-if __name__ == "__main__":
-
-    setSpeed(0.1)
-    turnLeft()
+npc.accelerate()
+npc.turnLeft()
+npc.brake()
