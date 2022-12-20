ec646973de021405f769495b118438334efefefc
Felix Ruf <felix.ruf@student.hs-rm.de>
2022-12-20 17:01:01 +0100

    BLI-11 #118 BUGFIX: fixed a small typo

--- vue/src/components/spawnpoint/SpawnPoint.vue
+++ vue/src/components/spawnpoint/SpawnPoint.vue
@@ -228,7 +228,7 @@ const testObj = ref({
 });
 
 onMounted(async () => {
-    // testObj.value = await getGameMap(props.instanceId);
+    testObj.value = await getGameMap(props.instanceId);
     const numberOfRows = testObj.value.tiles[0].length;
     addWindowWidthListener(numberOfRows);
 });
