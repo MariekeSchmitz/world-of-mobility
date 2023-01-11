2521855a05ea2dffb970904223c037cdd10f6026
Astrid Klemmer <astrid.klemmer@student.hs-rm.de>
2023-01-11 12:07:05 +0100

    BLI-157 #182 REFACTOR: Farm not walkable

--- spring/src/main/java/de/hsrm/mi/swt_project/demo/editor/tiles/FarmTile.java
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/editor/tiles/FarmTile.java
@@ -1,7 +1,5 @@
 package de.hsrm.mi.swt_project.demo.editor.tiles;
 
-import de.hsrm.mi.swt_project.demo.editor.tiles.tile_properties.Walkable;
-
-public class FarmTile extends Tile implements Walkable{
+public class FarmTile extends Tile{
     
 }
