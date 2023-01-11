d30246b31becbdf5e5df3fa50982f1ef02fadca7
Beate Arnold <beate.arnold@student.hs-rm.de>
2023-01-11 16:05:00 +0100

    BLI-28: FEATURE: add sideways models @CO-Author:Victoria Thee

---
+++ vue/src/components/Tiles/SIDEWAY_BICYCLE_CURVE.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+//@ts-ignore
+import { Vector3 } from "three";
+import { GltfModel } from "troisjs";
+import { withDefaults, defineProps } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    width: number;
+    height: number;
+    position: Vector3;
+    rotation: Vector3;
+    type: string;
+    placedObject: any;
+    orientation: string;
+  }>(),
+  { width: 10, height: 10 }
+);
+</script>
+<template>
+  <GltfModel
+    ref="model"
+    src="/src/assets/models/SIDEWAY_BICYCLE_CURVE.glb"
+    :position="props.position"
+    :rotation="props.rotation"
+  />
+</template>
