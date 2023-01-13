61414087b1aa4851875cc7927dc4eaf0ccb8f796
Astrid Klemmer <astrid.klemmer@student.hs-rm.de>
2023-01-13 09:59:49 +0100

    BLI-105 FEATURE: positioned choose avatar box

--- vue/src/components/User/Avatar_Selection.vue
+++ vue/src/components/User/Avatar_Selection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Chooseable_Avatar from "@/components/User/Chooseable_Avatar.vue";
 import { useLogin } from "@/services/login/useLogin";
- 
+
 /**
  * Component for Avatar Selection. Includes one Chooseable Avatar per available Avatar Choice
  * @author Tom Gouthier
@@ -9,7 +9,6 @@ import { useLogin } from "@/services/login/useLogin";
  */
 
 const { loginData, login, logout } = useLogin();
-
 
 /**
  * all available Avatars
@@ -23,30 +22,27 @@ const avatarPictures: string[] = [
   "src/assets/avatar/Schwein.png",
   "src/assets/avatar/avatar.png",
 ];
-
 </script>
 
 <template>
-  <div class="bg-white">
-    <h2 class="">Choose <br>your Avatar</h2>
-    <div class="grid grid-cols-3">
-
-      <div v-for="avatar in avatarPictures"
-        :key="avatarPictures.indexOf(avatar)">
-
-        <div v-if="avatar != loginData.avatar">
-          <Chooseable_Avatar
-            :avatarPicture="avatar"
-            :size="'s'" >
-          </Chooseable_Avatar>
+  <div
+    class="fixed inset-0 bg-orange bg-opacity-50 overflow-y-auto h-full w-full grid grid-cols-3 content-center"
+  >
+    <div class="bg-white p-16 grid col-start-2 col-end-2">
+      <h2 class="">Choose <br />your Avatar</h2>
+      <div class="grid grid-cols-3">
+        <div
+          v-for="avatar in avatarPictures"
+          :key="avatarPictures.indexOf(avatar)"
+        >
+          <div v-if="avatar != loginData.avatar">
+            <Chooseable_Avatar :avatarPicture="avatar" :size="'s'">
+            </Chooseable_Avatar>
+          </div>
         </div>
-
       </div>
-  
     </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vue/src/views/StartPage.vue
+++ vue/src/views/StartPage.vue
@@ -5,10 +5,10 @@ import Avatar from "@/components/User/Avatar.vue";
 import { useLogin } from "@/services/login/useLogin";
 import { ref, watch } from "vue";
 import router from "@/router";
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 // Then add it to library
-library.add(faPen)
+library.add(faPen);
 
 const { loginData, login, logout } = useLogin();
 
@@ -26,25 +26,22 @@ async function loginAndRedirect(url: string) {
   }
 }
 
-watch(loginData,
-  (neu, alt) => {
-    toggleAvatarSelection()
+watch(loginData, (neu, alt) => {
+  if (neu.avatar) {
+    toggleAvatarSelection();
   }
-)
-
-
+});
 </script>
 
 <template>
-
-  <div class="grid grid-cols-3 content-center h-screen box-border bg-[url('/src/assets/images/home.png')] bg-cover">
-
+  <div
+    class="grid grid-cols-3 content-center h-screen box-border bg-[url('/src/assets/images/home.png')] bg-cover"
+  >
     <div class="grid text-center col-start-2 col-end-2 justify-items-center">
-      
       <div class="">
-        <h1 class="">World of <br>E-Mobility</h1>
+        <h1 class="">World of <br />E-Mobility</h1>
       </div>
-    
+
       <!-- big Avatar
       <div>
         <Avatar :avatarPicture="loginData.avatar" :size="'l'"></Avatar>
@@ -52,39 +49,67 @@ watch(loginData,
 
       <!-- small Avatar with change function -->
       <div class="grid grid-cols-3 gap-6 items-center">
-
         <div class="grid justify-items-end items-end">
-          <Avatar :avatarPicture="loginData.avatar" class="inline w-28"></Avatar>
-          <button @click="toggleAvatarSelection" class="bg-orange absolute rounded-full w-11 h-11"><font-awesome-icon icon="fa-solid fa-pen" size="xl" color="white"/></button>
+          <Avatar
+            :avatarPicture="loginData.avatar"
+            class="inline w-28"
+          ></Avatar>
+          <button
+            @click="toggleAvatarSelection"
+            class="bg-orange absolute rounded-full w-11 h-11"
+          >
+            <font-awesome-icon icon="fa-solid fa-pen" size="xl" color="white" />
+          </button>
         </div>
-        
+
         <!-- input for username -->
-        <input v-if="!loginData.loggedIn" type="text" v-model="name" placeholder="Bitte Namen eingeben" class=" col-span-2   h-1/4 mr-12"/>
-        <div class="text-greenDark font-semibold text-left" v-if="loginData.loggedIn">{{ loginData.username }}</div>
+        <input
+          v-if="!loginData.loggedIn"
+          type="text"
+          v-model="name"
+          placeholder="Bitte Namen eingeben"
+          class="col-span-2 h-1/4 mr-12"
+        />
+        <div
+          class="text-greenLight font-semibold text-left"
+          v-if="loginData.loggedIn"
+        >
+          {{ loginData.username }}
+        </div>
 
-
-        <!-- <p v-if="loginData.error !== ''">{{ loginData.error }}</p> -->
-
+        <!--<p class="text-greenLight" v-if="loginData.error !== ''">
+          {{ loginData.error }}
+        </p>-->
       </div>
 
       <div class="w-1/2 mt-8">
-        <button class="buttonStartPage bg-orange" @click="loginAndRedirect('/gameintro') ">Spielmodus</button>
-        <button class="buttonStartPage bg-greenDark" @click="loginAndRedirect('/worldintro')">Baumodus</button>
+        <button
+          class="buttonStartPage bg-orange"
+          @click="loginAndRedirect('/gameintro')"
+        >
+          Spielmodus
+        </button>
+        <button
+          class="buttonStartPage bg-greenDark"
+          @click="loginAndRedirect('/worldintro')"
+        >
+          Baumodus
+        </button>
       </div>
-      
+
       <!-- Avatar Selection Mode -->
-      <Avatar_Selection class="" v-if="choosingAvatar"></Avatar_Selection>
-      
+      <Avatar_Selection v-if="choosingAvatar"></Avatar_Selection>
     </div>
-    <button v-if="loginData.loggedIn" class= "text-white absolute -right-0 m-10 py-3 px-5 bg-orange rounded-full font-poppins font-semibold" @click="logout()">Logout</button>
-  
+    <button
+      v-if="loginData.loggedIn"
+      class="text-white absolute -right-0 m-10 py-3 px-5 bg-orange rounded-full font-poppins font-semibold"
+      @click="logout()"
+    >
+      Logout
+    </button>
+
     <!-- <div class="fixed block inset-0 bg-orange bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal"></div> -->
-  
   </div>
-
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
