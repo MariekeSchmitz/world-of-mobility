4b16cb6099eed1ab791293d19b7255b7b2304356
Sascha Scheid <sascha.scheid@student.hs-rm.de>
2023-01-28 13:48:47 +0100

    FEATURE: Add classes that provide restricted access to Tiles and Placeable

---
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/scripting/PlaceableProxy.java
@@ -0,0 +1,36 @@
+package de.hsrm.mi.swt_project.demo.scripting;
+
+import de.hsrm.mi.swt_project.demo.editor.placeableobjects.Placeable;
+import de.hsrm.mi.swt_project.demo.editor.placeableobjects.PlaceableObjectType;
+
+/**
+ * This class restricts access to the Tile class,
+ * so that users are able to access but not change
+ * the tile properties.
+ * 
+ * @author Sascha Scheid
+ * @author Finn Schindel
+ */
+public class PlaceableProxy {
+    
+    protected Placeable placeable;
+
+    /**
+     * Creates a new PlaceableProxy.
+     * 
+     * @param placeable Placeable to restrict access to.
+     */
+    public PlaceableProxy(Placeable placeable) {
+        this.placeable = placeable;
+    } 
+
+    /**
+     * Gets type of the placeable object.
+     * 
+     * @return Type of the placeable object.
+     */
+    public PlaceableObjectType getType() {
+        return this.placeable.getType();
+    }
+
+}
---
+++ spring/src/main/java/de/hsrm/mi/swt_project/demo/scripting/TileProxy.java
@@ -0,0 +1,72 @@
+package de.hsrm.mi.swt_project.demo.scripting;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.hsrm.mi.swt_project.demo.controls.Orientation;
+import de.hsrm.mi.swt_project.demo.editor.placeableobjects.Placeable;
+import de.hsrm.mi.swt_project.demo.editor.tiles.Tile;
+import de.hsrm.mi.swt_project.demo.editor.tiles.Tiletype;
+import de.hsrm.mi.swt_project.demo.editor.tiles.TrafficTile;
+
+/**
+ * This class restricts access to the Tile class,
+ * so that users are able to access but not change
+ * the tile properties.
+ * 
+ * @author Sascha Scheid
+ * @author Finn Schindel
+ */
+public class TileProxy {
+    
+    protected Tile tile;
+
+    /**
+     * Creates new TileProxy.
+     * 
+     * @param tile Tile to restrict access to.
+     */
+    public TileProxy(Tile tile) {
+        this.tile = tile;
+    }
+
+    /**
+     * Gets type of the tile.
+     * 
+     * @return Type of the tile.
+     */
+    public Tiletype getType() {
+        return this.tile.getType();
+    }
+
+    /**
+     * Gets restricted proxy of the placeable
+     * object that is placed on the tile.
+     * 
+     * @return Proxy for the placeable on the tile.
+     */
+    public PlaceableProxy getPlaceable() {
+        Placeable placeable = this.tile.getPlacedObject();
+        return new PlaceableProxy(placeable);
+    }
+
+    /**
+     * Gets all orientations from which movement is allowed
+     * to the tile.
+     * 
+     * @return Specified directions for traffic tiles, all
+     *         existing directions for any other tile.
+     */
+    public List<Orientation> getAllowedOrientations() {
+
+        if (tile instanceof TrafficTile trafficTile) {
+            return trafficTile.getAllowedDirections();
+        }
+
+        // Movement on non-traffic tiles is allowed 
+        // from any direction
+        return Arrays.asList(Orientation.values());
+    }
+
+    
+}
